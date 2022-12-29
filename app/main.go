f9797bd2d4abfd9a40114babb01600468ef2b5db
Julian <unfehlerhaft@gmail.com>
2022-12-29 22:49:27 +0100

    Hello, World!

---
+++ app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	// Listen on port 8880
+	app.Listen(":8880")
+}
