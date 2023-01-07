c66baaae66200404979c2a181574debb93ec8e73
Julian <unfehlerhaft@gmail.com>
2023-01-07 18:34:33 +0100

    New structure

--- src/auth/mod.rs
+++
@@ -1,8 +0,0 @@
-mod auth;
-
-use actix_web::web;
-
-pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(auth::login)
-    .service(auth::register);
-}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
-pub mod auth;
+use actix_web::{App, HttpServer, Responder, get, web};
+use sea_orm::{DatabaseConnection, Database};
 
-use actix_web::{web, App, HttpServer};
+#[get("/test")]
+async fn test(data: web::Data<AppState>) -> impl Responder {
+    "Hello world!"
+}
+
+// Database config
+const DATABASE_URL: &str = "postgres://postgres:deinemutter123@localhost/learn";
+
+#[derive(Debug, Clone)]
+struct AppState {
+    conn: DatabaseConnection,
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+
+    let conn = Database::connect(DATABASE_URL).await.unwrap();
+
+    println!("Connected to database: {}", DATABASE_URL);
+
+    let state = AppState { conn };
+
+    HttpServer::new(move || {
         App::new()
-            .service(web::scope("/auth").configure(auth::config))
+            .app_data(web::Data::new(state.clone()))
+            .service(test)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
