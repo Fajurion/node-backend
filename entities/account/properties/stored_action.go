05fe15c15d70d34f7d06d7f5a4399fd2ac092f4c
Julian <unfehlerhaft@gmail.com>
2023-08-14 17:33:00 +0200

    Changes

--- entities/account/properties/stored_action.go
+++ entities/account/properties/stored_action.go
@@ -6,3 +6,11 @@ type StoredAction struct {
 	Account string `json:"-" gorm:"not null"`
 	Payload string `json:"action" gorm:"not null"` // Encrypted payload (encrypted with the account's public key)
 }
+
+// TODO: Implement some sort of key that one can send to friends for them to be able to send those actions
+type AuthenticatedStoredAction struct {
+	ID string `json:"-" gorm:"primaryKey"`
+
+	Account string `json:"-" gorm:"not null"`
+	Payload string `json:"action" gorm:"not null"` // Encrypted payload (encrypted with the account's public key)
+}
